5cd62f02fb966cab24337e9c2c7730f47fd43553
Gabriel K <96412510+Kl1p@users.noreply.github.com>
2022-05-27 09:30:48 +0000

    Update imgscrapper.py

--- imgscrapper.py
+++ imgscrapper.py
@@ -3,34 +3,41 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import datetime
-                                        ################## CODE ###############
+                                        
 
 
-                    ## FUNCTION ##
+                     ## PRIMARY FUNCTION ##
 
 def gtimg(x,tags):#x = page number // tags == specific tag.
     
     url = f'yoururlhere?page={x}&tags={tags}' # adjust page and tags(if any) for your needs
+    
     r = requests.get(url) # sends a GET request to your url 
+    
     sp = BeautifulSoup(r.content, 'html.parser')  
+    
     images = sp.find_all('a',class_='directlink largeimg') #Select div/class
     for image in images:
-        print(image['href'])#ITINERATE INSIDE THE CLASS SEARCHING FOR THE LINK.
-    
+        print(image['href']) #ITINERATE INSIDE THE CLASS SEARCHING FOR THE LINK.
     return
                     ## SESSION ##
 
-def gtimg_session(x,tags):#Makes it work faster by using the same request for the page you are in,
-                                #instead of sending a new request for every new image downloaded
+def gtimg_session(x,tags): #Makes it work faster by using the same request for the page you are in,
+                                 #instead of sending a new request for every new image downloaded
 
     url = f'yoururlhere?page={x}&tags={tags}' 
+    
     r = s.get(url)#STARTS THE SESSION
+    
     sp = BeautifulSoup(r.content, 'html.parser')
     images = sp.find_all('a',class_='directlink largeimg')
+    
     for image in images:
             zx = image['href'] 
             response = requests.get(zx) 
+            
             print(f'Still Downloading... Page:',x,'Statuscode:',response.status_code) 
+            
             filename = r'urfolderhere' + zx[zx.rfind('/'):]  #create the folder//name it
             with open(filename, 'wb') as f: 
                 for chunk in response.iter_content(chunk_size=128): #builds the img
@@ -42,10 +49,13 @@ if __name__ == '__main__': # execute the first function
     s = requests.session()
     start = datetime.datetime.now()
     tags = input('Tag: ') # LEAVE BLANK IF NONE
+    
     page1 = int(input('First page: '))
     page2 = int(input('Second page: '))
+    
     for x in range(page1,page2):
         gtimg_session(x,tags)
+    
     finish = datetime.datetime.now() - start
     print(finish)
     total =(page2-page1)
